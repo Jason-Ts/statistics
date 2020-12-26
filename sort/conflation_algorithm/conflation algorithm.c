19c85bbabf25e0e328818d42f9814f8a4fce345a
Jason-Ts <54155246+Jason-Ts@users.noreply.github.com>
2020-12-26 22:18:45 +0800

    Add files via upload

--- sort/conflation_algorithm/conflation algorithm.c
+++ sort/conflation_algorithm/conflation algorithm.c
@@ -1,3 +1,5 @@
+#if !defined(CONFLATION_ALGORITHM)
+#define CONFLATION_ALGORITHM
 /*
 conflatioin algorithm
 n:  length of the array
@@ -8,25 +10,11 @@ ascend: ascending order or not
 void conflation(long int n,double array[n],double output[n],int ascend)
 {
     long int i=0,i1=0,i2=0,n1=n/2,n2=n-n1;
-    double a1[n1],a2[n2],o1[n1],o2[n2];
-    // divide
-    for (;i<n1;i++)
-        a1[i]=array[i];
-    for (i=0;i<n2;i++)
-        a2[i]=array[i+n1];
+    double *a1,*a2,o1[n1],o2[n2];
     // sort
     if (n==1) output[0]=array[0];
-    else if (n==2){
-        if (ascend){
-            output[0]=(array[0]<array[1]) ? array[0] : array[1];
-            output[1]=(array[0]>array[1]) ? array[0] : array[1];
-        }
-        else{
-            output[0]=(array[0]>array[1]) ? array[0] : array[1];
-            output[1]=(array[0]<array[1]) ? array[0] : array[1];
-        }
-    }
     else{
+        a1=&array[0];a2=&array[n1];
         conflation(n1,a1,o1,ascend);
         conflation(n2,a2,o2,ascend);
         for (i=0;i<n;i++){
@@ -60,3 +48,5 @@ void conflation(long int n,double array[n],double output[n],int ascend)
                 output[i]=o1[i1];
     }
 }
+
+#endif // CONFLATION_ALGORITHM
